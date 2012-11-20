e7afd263fa06da4a89669b1df93feba8e570ff37
Rafal Lewczuk <rafal.lewczuk@gmail.com>
2012-11-20 20:47:03 +0100

    Rudimentary ThreadRank code (still not tested nor directly available from BSH).

---
+++ zorka-agent/src/main/java/com/jitlogic/zorka/rankproc/ThreadRankLister.java
@@ -0,0 +1,132 @@
+/**
+ * Copyright 2012 Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
+ * <p/>
+ * This is free software. You can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p/>
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.jitlogic.zorka.rankproc;
+
+import com.jitlogic.zorka.agent.AgentInstance;
+import com.jitlogic.zorka.agent.MBeanServerRegistry;
+import com.jitlogic.zorka.util.ZorkaUtil;
+
+import java.lang.management.ManagementFactory;
+import java.lang.management.ThreadInfo;
+import java.lang.management.ThreadMXBean;
+import java.util.*;
+
+public class ThreadRankLister implements Runnable, RankLister<ThreadRankItem> {
+
+    private MBeanServerRegistry mBeanServerRegistry;
+    private ThreadMXBean threadMXBean;
+    private Map<Long,ThreadRankItem> threads = new HashMap<Long, ThreadRankItem>();
+
+    private long interval;
+
+
+    public ThreadRankLister() {
+        this(14000);
+    }
+
+
+    public ThreadRankLister(long interval) {
+        this.interval = interval;
+        this.mBeanServerRegistry = AgentInstance.getMBeanServerRegistry();
+    }
+
+
+    public synchronized List<ThreadRankItem> list() {
+        List<ThreadRankItem> lst = new ArrayList<ThreadRankItem>(threads.size()+2)     ;
+
+        for (Map.Entry<Long,ThreadRankItem> e : threads.entrySet()) {
+            lst.add(e.getValue());
+        }
+
+        return Collections.unmodifiableList(lst);
+    }
+
+
+    protected ThreadInfo[] rawList() {
+        // Platform MBean Server startup might be
+        if (threadMXBean == null) {
+            if (mBeanServerRegistry.lookup("java") != null) {
+                threadMXBean = ManagementFactory.getThreadMXBean();
+            } else {
+                return new ThreadInfo[0];
+            }
+        }
+
+        return threadMXBean.getThreadInfo(threadMXBean.getAllThreadIds());
+    }
+
+
+    public synchronized void runCycle(long tstamp) {
+        ThreadInfo[] raw = rawList();
+        int sz = Math.max(raw.length, threads.size());
+        Map<Long,ThreadRankItem> newThreads = new HashMap<Long, ThreadRankItem>(sz*2+10, 0.5f);
+
+        for (ThreadInfo threadInfo : raw) {
+            if (threadInfo == null) {
+                continue;
+            }
+
+            long tid = threadInfo.getThreadId();
+
+            ThreadRankItem threadItem = threads.get(threadInfo.getThreadId());
+            if (threadItem == null) {
+                threadItem = new ThreadRankItem(threadInfo);
+            }
+
+            threadItem.feed(tstamp, threadInfo, threadMXBean.getThreadUserTime(tid));
+            newThreads.put(threadInfo.getThreadId(), threadItem);
+        }
+
+        threads = newThreads;
+
+    }
+
+
+    private volatile boolean started = false;
+    private volatile Thread thread = null;
+
+
+    public void run() {
+        while (started) {
+            runCycle(System.currentTimeMillis());
+            try {
+                Thread.sleep(interval);
+            } catch (InterruptedException e) { }
+
+        }
+
+        thread = null;
+    }
+
+
+    public synchronized void start() {
+        if (!started) {
+            thread = new Thread(this);
+            thread.setDaemon(true);
+            thread.setName("Zorka-thread-lister");
+            thread.start();
+        }
+    }
+
+
+    public synchronized void stop() {
+        if (started) {
+            thread.interrupt();
+            started = false;
+        }
+    }
+
+}
