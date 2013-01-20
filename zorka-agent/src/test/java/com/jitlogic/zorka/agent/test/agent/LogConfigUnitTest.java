e8ca1211dbfb42dbef18d94e85c3fe2fa14e58ad
Rafal Lewczuk <rafal.lewczuk@gmail.com>
2013-01-20 21:17:03 +0100

    Fix zorka-agent unit test package layout.

---
+++ zorka-agent/src/test/java/com/jitlogic/zorka/agent/test/agent/LogConfigUnitTest.java
@@ -0,0 +1,34 @@
+/**
+ * Copyright 2012-2013 Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
+ * <p/>
+ * This is free software. You can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p/>
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package com.jitlogic.zorka.agent.test.agent;
+
+import com.jitlogic.zorka.common.ZorkaLogConfig;
+import org.junit.Test;
+import org.junit.Assert;
+
+public class LogConfigUnitTest {
+
+    @Test
+    public void testParseSimpleLogConfigStrings() {
+        Assert.assertEquals(ZorkaLogConfig.ZTR_CONFIG, ZorkaLogConfig.parse("", "ZTR", "CONFIG"));
+        Assert.assertEquals(ZorkaLogConfig.ZTR_CONFIG|ZorkaLogConfig.ZTR_TRACE_CALLS,
+                ZorkaLogConfig.parse("", "ZTR", "CONFIG,TRACE_CALLS"));
+        Assert.assertEquals(ZorkaLogConfig.ZTR_CONFIG|ZorkaLogConfig.ZTR_TRACE_CALLS,
+                ZorkaLogConfig.parse("", "ZTR", "config, trace_calls"));
+    }
+
+}
---
+++ zorka-agent/src/test/java/com/jitlogic/zorka/agent/test/rankproc/ThreadRankUnitTest.java
@@ -0,0 +1,91 @@
+/**
+ * Copyright 2012-2013 Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
+ * <p/>
+ * This is free software. You can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p/>
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.jitlogic.zorka.agent.test.rankproc;
+
+import com.jitlogic.zorka.agent.test.rankproc.support.TestThreadRankLister;
+import com.jitlogic.zorka.agent.test.support.ZorkaFixture;
+
+import com.jitlogic.zorka.agent.rankproc.ThreadRankItem;
+import com.jitlogic.zorka.agent.rankproc.ThreadRankLister;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+import java.util.List;
+
+
+
+public class ThreadRankUnitTest extends ZorkaFixture {
+
+    @Test
+    public void testListSinglePass() {
+        ThreadRankLister lister = new TestThreadRankLister()
+                .feed(1, "Thread-1", 100, 0)
+                .feed(2, "Thread-2", 100, 0);
+
+        lister.runCycle(100);
+
+        List<ThreadRankItem> items = lister.list();
+        assertEquals(2, items.size());
+    }
+
+
+    @Test
+    public void testTwoPassesAndCheckIfNonExistentThreadsAreRemoved() {
+        TestThreadRankLister lister = new TestThreadRankLister()
+                .feed(1, "Thread-1", 100, 0)
+                .feed(2, "Thread-2", 100, 0);
+
+        lister.runCycle(1000);
+
+        lister.clear()
+                .feed(1, "Thread-1", 100, 0)
+                .feed(3, "Thread-3", 100, 0);
+
+        lister.runCycle(2000);
+
+        List<ThreadRankItem> items = lister.list();
+
+        assertEquals(2, items.size());
+    }
+
+
+    @Test
+    public void testTwoPassesAndCheckAverages() {
+        TestThreadRankLister lister = new TestThreadRankLister().feed(1, "Thread-1", 1000, 500);
+        lister.runCycle(1000);
+        lister.clear().feed(1, "Thread-1", 2000, 1500);
+        lister.runCycle(2000);
+
+        ThreadRankItem item = lister.list().get(0);
+        assertEquals(5.0, item.getAverage(0L, 0, 0), 0.001);
+    }
+
+
+    @Test
+    public void testTwoPassesWithWindowShift() {
+        TestThreadRankLister lister = new TestThreadRankLister().feed(1, "Thread-1", 6000, 0);
+        lister.runCycle(1000);
+
+        assertEquals(10.0, lister.list().get(0).getAverage(0L, 0,0), 0.001);
+
+        lister.clear().feed(1, "Thread-1", 1500, 0);
+        lister.runCycle(31000);
+
+        assertEquals(12.5, lister.list().get(0).getAverage(0L, 0,0), 0.001);
+        assertEquals(2.5, lister.list().get(0).getAverage(0L, 0,1), 0.001);
+    }
+}
