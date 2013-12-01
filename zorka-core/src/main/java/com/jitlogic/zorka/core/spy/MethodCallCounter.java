72aa2c62a822db1617485b3786a27a17f83db850
Rafal Lewczuk <rafal.lewczuk@gmail.com>
2013-12-01 22:10:32 +0100

    Method call counters for tracer (partial implementation).

---
+++ zorka-core/src/main/java/com/jitlogic/zorka/core/spy/MethodCallCounter.java
@@ -0,0 +1,155 @@
+/**
+ * Copyright 2012-2013 Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
+ * <p/>
+ * This is free software. You can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p/>
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.jitlogic.zorka.core.spy;
+
+
+import com.jitlogic.zorka.common.tracedata.MethodCallCounterRecord;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MethodCallCounter {
+
+    private static final int INITIAL_SIZE = 64;
+
+    private long keys[], vals[];
+
+    private long initialTime = 0L;
+
+    private int numEntries = 0, maxEntries = 0;
+
+    private static final long FREE = 0;
+    private static final long MASK = 0x1fffffL;
+
+    public MethodCallCounter() {
+        keys = new long[INITIAL_SIZE];
+        vals = new long[INITIAL_SIZE];
+        numEntries = 0;
+        maxEntries = INITIAL_SIZE * 3 / 4;
+    }
+
+
+    public void logCall(int classId, int methodId, int signatureId, long tstamp) {
+        logCalls(classId, methodId, signatureId, tstamp, 1);
+    }
+
+
+    public void logCalls(int classId, int methodId, int signatureId, long tstamp, long nCalls) {
+        long key = classId | (((long) methodId) << 21) | (((long) signatureId) << 42);
+
+        if (initialTime == tstamp) {
+            initialTime = tstamp;
+        }
+
+        if (numEntries > maxEntries) {
+            rehash(maxEntries * 2);
+        }
+
+        int idx = index(key);
+
+        if (keys[idx] == FREE) {
+            keys[idx] = key;
+            vals[idx] = nCalls;
+            numEntries++;
+        } else {
+            vals[idx] += nCalls;
+        }
+    }
+
+
+    public List<MethodCallCounterRecord> getRecords() {
+        List<MethodCallCounterRecord> ret = new ArrayList<MethodCallCounterRecord>();
+
+        for (int i = 0; i < keys.length; i++) {
+            long key = keys[i];
+            if (key != FREE) {
+                ret.add(new MethodCallCounterRecord((int) (key & MASK), (int) ((key >> 21) & MASK), (int) ((key >> 42) & MASK), vals[i]));
+            }
+        }
+
+        return ret;
+    }
+
+
+    public void sum(MethodCallCounter mcc) {
+        long[] mkeys = mcc.keys;
+        long[] mvals = mcc.vals;
+        long tst = mcc.initialTime;
+
+        for (int i = 0; i < mkeys.length; i++) {
+            long key = mkeys[i];
+            if (key != FREE) {
+                logCalls((int) (key & MASK), (int) ((key >> 21) & MASK), (int) ((key >> 42) & MASK), tst, mvals[i]);
+            }
+        }
+    }
+
+
+    public void clear() {
+        initialTime = 0L;
+        numEntries = 0;
+        for (int i = 0; i < keys.length; i++) {
+            keys[i] = 0;
+        }
+    }
+
+
+    private int lhash(long l) {
+        int v = (int) (l ^ (l >> 32));
+        return v >= 0 ? v : -v;
+    }
+
+
+    private void rehash(int capacity) {
+        int olen = keys.length;
+
+        long[] okeys = keys;
+        long[] ovals = vals;
+
+        long[] nkeys = new long[capacity];
+        long[] nvals = new long[capacity];
+
+        keys = nkeys;
+        vals = nvals;
+        maxEntries = capacity * 3 / 4;
+
+        for (int i = 0; i < olen; i++) {
+            long key = okeys[i];
+            int idx = index(key);
+            nkeys[idx] = key;
+            nvals[idx] = ovals[i];
+        }
+    }
+
+
+    private int index(long key) {
+        long[] keys = this.keys;
+        int length = keys.length;
+
+        int hash = lhash(key);
+        int i = hash % length;
+
+        if (i < 0) {
+            System.out.println("kurwa!");
+        }
+
+        while (keys[i] != FREE && keys[i] != key) {
+            i = (i > 0) ? i - 1 : length - 1;
+        }
+
+        return i;
+    }
+}
---
+++ zorka-core/src/test/java/com/jitlogic/zorka/core/test/spy/MethodCallCountingUnitTest.java
@@ -0,0 +1,94 @@
+/**
+ * Copyright 2012-2013 Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
+ * <p/>
+ * This is free software. You can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later
+ * version.
+ * <p/>
+ * This software is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ * <p/>
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, see <http://www.gnu.org/licenses/>.
+ */
+package com.jitlogic.zorka.core.test.spy;
+
+import com.jitlogic.zorka.common.tracedata.MethodCallCounterRecord;
+import com.jitlogic.zorka.core.spy.MethodCallCounter;
+import com.jitlogic.zorka.core.test.support.ZorkaFixture;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+import java.util.*;
+
+
+public class MethodCallCountingUnitTest extends ZorkaFixture {
+
+    @Test
+    public void testMethodCallCounterLoggingCalls() throws Exception {
+        Random r = new Random();
+
+        List<MethodCallCounterRecord> recs = new ArrayList<MethodCallCounterRecord>();
+        List<Integer> counters = new ArrayList<Integer>();
+
+        // Generate some initial data
+        for (int i = 0; i < 10000; i++) {
+
+            MethodCallCounterRecord rec = new MethodCallCounterRecord(
+                    r.nextInt(1500000) + 1, r.nextInt(1500000) + 2, r.nextInt(1500000) + 3, r.nextInt(10) + 1);
+
+            recs.add(rec);
+            counters.add(0);
+        }
+
+        MethodCallCounter mcc = new MethodCallCounter();
+
+        // Fill in method call counter
+        while (true) {
+            int incs = 0;
+
+            for (int i = 0; i < recs.size(); i++) {
+                MethodCallCounterRecord rec = recs.get(i);
+                if (counters.get(i) < rec.getnCalls()) {
+                    mcc.logCall(rec.getClassId(), rec.getMethodId(), rec.getSignatureId(), 10L);
+                    counters.set(i, counters.get(i) + 1);
+                    incs++;
+                }
+            }
+
+            if (incs == 0) {
+                break;
+            }
+        }
+
+        Set<MethodCallCounterRecord> results = new HashSet<MethodCallCounterRecord>();
+        results.addAll(mcc.getRecords());
+
+        // Indicate (potential) errors
+        for (MethodCallCounterRecord mcr : recs) {
+            if (!results.contains(mcr)) {
+                MethodCallCounterRecord mcrr = null;
+                for (MethodCallCounterRecord mcr2 : results) {
+                    if (mcr2.getClassId() == mcr.getClassId() && mcr2.getMethodId() == mcr.getMethodId()
+                            && mcr2.getSignatureId() == mcr.getSignatureId()) {
+                        mcrr = mcr2;
+                        break;
+                    }
+                }
+                System.out.println("Results mismatch: " + mcr + " <-> " + mcrr);
+            }
+        }
+
+        // Check results correctness
+        assertEquals("Number of registered records should be the same.", recs.size(), results.size());
+
+        for (MethodCallCounterRecord mcr : recs) {
+            assertTrue("Result should contain record " + mcr, results.contains(mcr));
+        }
+    }
+
+}
