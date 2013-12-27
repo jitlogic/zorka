3a82abc0027f37b8ec4c28ac676c922ca0610d2f
Rafal Lewczuk <rafal.lewczuk@gmail.com>
2013-12-27 08:23:50 +0100

    Collector data migration script (from old MySQL-backed to current format).

---
+++ zico-dist/src/migrate.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+
+import MySQLdb, json
+from os.path import isdir, exists
+from os import rename
+
+db = MySQLdb.connect('localhost', 'zorka', 'zorka', 'zorka')
+
+# Export symbol table (to be used later)
+c = db.cursor()
+c.execute('select SID,NAME from SYMBOLS')
+symbols = { }
+
+for r in c:
+    symbols[r[0]] = r[1]
+
+
+c.close()
+
+# Export and prepare all hosts (to be finished inside collector itself)
+
+hosts = { }
+
+c = db.cursor()
+c.execute('select HOST_NAME,HOST_ADDR,HOST_PATH,HOST_FLAGS,MAX_SIZE,HOST_PASS,HOST_ID from HOSTS')
+
+HOST_PROPS = """
+flags=%d
+size=%d
+pass=%s
+addr=%s
+"""
+
+for r in c:
+  h = r[2]
+  hosts[r[6]] = r[0]
+  if isdir("data/%s" % h):
+    if not exists("data/%s/host.properties"%h):
+      print "Generating data/%s/host.properties" % h
+      with open("data/%s/host.properties"%h, "w") as f:
+        f.write(HOST_PROPS % (7,r[4],r[5],r[1]))
+    if not exists("data/%s/symbols.dat"):
+      print "Generating data/%s/symbols.json" % h
+      with open("data/%s/symbols.json"%h,"w") as f:
+        f.write(json.dumps(symbols))
+    if isdir("data/%s/traces" % h) and not isdir("data/%s/tdat" % h):
+      print "Renaming data/%s/traces to data/%s/tdat" % (h,h)
+      rename("data/%s/traces"%h, "data/%s/tdat"%h)
+
+c.close()
+
+# 
+c = db.cursor()
+c.execute("select USER_ID,USER_NAME,REAL_NAME,FLAGS,PASSWORD from USERS")
+
+users = { }
+
+for r in c:
+  users[r[0]] = { "user_name": r[1], "real_name": r[2], "flags": r[3], "password": r[4], "hosts": [ ] }
+
+c.close()
+
+c = db.cursor()
+c.execute("select USER_ID,HOST_ID from USERS_HOSTS")
+
+for r in c:
+  if r[0] in users and r[1] in hosts:
+    users[r[0]]["hosts"].append(hosts[r[1]])
+
+c.close()
+
+if not exists("users.db") and not exists("users.json"):
+  print "Generating user list ..."
+  with open("users.json", "w") as f:
+    f.write(json.dumps(users))
+
+
+db.close()
+
+
+
+
+
+#print json.dumps(symbols)
