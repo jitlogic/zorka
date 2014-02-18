05b32e49205be103e2d3d5fb4073dbcbf4f58a24
Rafal Lewczuk <rafal.lewczuk@gmail.com>
2014-02-18 16:53:53 +0100

    Simple regex matching functions in UtilLib.

--- zorka-core/src/main/java/com/jitlogic/zorka/core/UtilLib.java
+++ zorka-core/src/main/java/com/jitlogic/zorka/core/UtilLib.java
@@ -27,6 +27,7 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
+import java.util.regex.Pattern;
 
 public class UtilLib {
 
@@ -130,4 +131,11 @@ public class UtilLib {
         return new JSONWriter().write(obj);
     }
 
+    public Pattern rePattern(String pattern) {
+        return Pattern.compile(pattern);
+    }
+
+    public boolean reMatch(Pattern pattern, String s) {
+        return pattern.matcher(s).matches();
+    }
 }
