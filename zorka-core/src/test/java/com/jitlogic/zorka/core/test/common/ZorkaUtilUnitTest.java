4ecb7b2ace60a2b23399c1e3f4a944daf6ad7455
Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
2019-01-19 14:53:44 +0100

    Update copyrights.

--- zorka-core/src/test/java/com/jitlogic/zorka/core/test/common/ZorkaUtilUnitTest.java
+++ zorka-core/src/test/java/com/jitlogic/zorka/core/test/common/ZorkaUtilUnitTest.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2018 Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
+ * Copyright 2012-2019 Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
  * <p/>
  * This is free software. You can redistribute it and/or modify it under the
  * terms of the GNU Lesser General Public License as published by the Free Software
