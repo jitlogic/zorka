79511cc6fed82ba82c0c18c8e158e97298deee90
Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
2019-02-08 21:20:20 +0100

    Update: ASM 6.2 -> 7.0.

--- zorka-core/src/main/java/com/jitlogic/zorka/core/spy/SpyClassVisitor.java
+++ zorka-core/src/main/java/com/jitlogic/zorka/core/spy/SpyClassVisitor.java
@@ -87,7 +87,7 @@ public class SpyClassVisitor extends ClassVisitor {
     public SpyClassVisitor(SpyClassTransformer transformer, ClassLoader classLoader, SymbolRegistry symbolRegistry,
                            String className,
                            List<SpyDefinition> sdefs, Tracer tracer, ClassVisitor cv) {
-        super(Opcodes.ASM5, cv);
+        super(Opcodes.ASM7, cv);
         this.transformer = transformer;
         this.classLoader = classLoader;
         this.symbolRegistry = symbolRegistry;
