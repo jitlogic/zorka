de865b6e8cc021696108d3dbc689ff37c33da113
Rafal Lewczuk <rafal.lewczuk@jitlogic.com>
2020-02-11 13:22:02 +0100

    Fix: race conditions in MemoryChunkStore.addAll() + other fixes.

--- zorka-common/src/main/java/com/jitlogic/zorka/common/collector/MemoryChunkStore.java
+++ zorka-common/src/main/java/com/jitlogic/zorka/common/collector/MemoryChunkStore.java
@@ -1,5 +1,8 @@
 package com.jitlogic.zorka.common.collector;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
@@ -10,6 +13,8 @@ import static com.jitlogic.zorka.common.util.ZorkaUtil.MB;
 
 public class MemoryChunkStore implements TraceChunkStore {
 
+    private static Logger log = LoggerFactory.getLogger(MemoryChunkStore.class);
+
     private long maxSize = 4 * GB;
     private long delSize = 256 * MB;
     private long curSize = 0;
@@ -18,11 +23,14 @@ public class MemoryChunkStore implements TraceChunkStore {
 
     public MemoryChunkStore() { }
 
-    public MemoryChunkStore(long maxSize) {
-        this.maxSize = maxSize;
+    public MemoryChunkStore(long maxSize, long delSize) {
+        this.maxSize = Math.max(maxSize, 512 * MB);
+        this.delSize = delSize;
+        log.info("Initialized memory store: maxSize={}->{}, delSize={}", maxSize, this.maxSize, delSize);
     }
 
     private void trim() {
+        long t1 = System.currentTimeMillis();
         int sz = 0, idx = -1;
         for (int i = 0; i < chunks.size() && sz < delSize; i++) {
             sz += chunks.get(i).size();
@@ -35,6 +43,8 @@ public class MemoryChunkStore implements TraceChunkStore {
         }
         long csz = 0;
         for (TraceChunkData chunk : chunks) csz += chunk.size();
+        long t2 = System.currentTimeMillis();
+        log.info("Trimming chunk store size: {} -> {} (t={}ms)", curSize, csz, (t2-t1));
         curSize = csz;
     }
 
@@ -56,7 +66,7 @@ public class MemoryChunkStore implements TraceChunkStore {
         while (curSize > maxSize) trim();
     }
 
-    public Set<String> attrVals(String attrName) {
+    public synchronized Set<String> attrVals(String attrName) {
         Set<String> rslt = new HashSet<String>();
         for (TraceChunkData c : chunks) {
             String s = c.getAttr(attrName);
@@ -65,16 +75,16 @@ public class MemoryChunkStore implements TraceChunkStore {
         return rslt;
     }
 
-    public TraceChunkData get(int idx) {
+    public synchronized TraceChunkData get(int idx) {
         return chunks.get(idx);
     }
 
-    public List<TraceChunkData> getChunks() {
-        return chunks;
+    public synchronized List<TraceChunkData> getChunks() {
+        return new ArrayList<TraceChunkData>(chunks);
     }
 
     /** Returns number of chunks */
-    public int length() {
+    public synchronized int length() {
         return chunks.size();
     }
 }
